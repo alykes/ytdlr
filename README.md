bae0d9ccc53c564b09b9b1610b7401566986d8ef
alykes <anthony.gouskos@gmail.com>
2021-01-26 14:17:28 +1100

    Having a working application that lists what streams are available for a youtube video. List can be sorted by audio or video (mp4; 720 or 1080).

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ### Youtube Downloader (ytdlr)
 
-This is a little tool that I have been meaning to write for a very long time. There are a multitude of sites out there that give you this ability.
-Some of the sites are innocent but a lot of them are very nefarious, I also don't like the idea of downloading a random executable that may or may not
-contain malicious code.
+This is a little tool that I have been meaning to write for a very long time. There are a multitude of sites out there that give you the ability to download audio and videos from YouTube.
 
-So with those thoughts in might, I wanted to create something small for myself, with code that is easily viewable for the denizens of the internet.
+I don't know what sits behind these web services. I also don't like the idea of downloading a random executable that may or may not
+contain malicious code for the sake of grabbing a video or an audio track.
+
+So with those thoughts in might, I wanted to create something small for myself, with code that I can track. If anyone else finds this useful as well, then great!
+
+Let me know what else I can include or improve on.
 
 Anyway, enough gum flappin'
 
@@ -15,7 +18,8 @@ Ensure that you download the pytube3 package for this python script to work
 pip install pytube3
 ```
 
-There is a currently know bug, your best bet for getting pytube3 to run is by executing the following commands
+**NOTE**
+There is currently a known issue with version available on pip, your best bet for getting pytube3 to run is below
 ```
 pip3 uninstall -y pytube3
 pip3 install git+https://github.com/nficano/pytube
--- python/main.py
+++ python/main.py
@@ -1,14 +1,24 @@
 from tkinter import *
 from pytube import YouTube
+from pytube.helpers import safe_filename
 
-def dlit():
-    url = textbox.get()
-    output.delete(0.0, END) #first line first character and delete til end
+def ListStreams():
+    url = TextBox.get()
     #I need to include a check on the text entered to ensure it has a www.youtube.com
     yt = YouTube(url)
     #yt = YouTube('https://www.youtube.com/watch?v=yUdxHAhj8l8')
-    sort = yt.streams
-    output.insert(END, sort)
+    output.delete(0.0, END) #delete first character on the first line til the end of the textbox
+    audio_flag = check.get()
+    #StreamList = yt.streams
+    for stream in yt.streams.filter(only_audio=audio_flag):
+        if audio_flag:
+            StreamList = f'itag: {stream.itag} Codec: {stream.audio_codec} BitRate: {stream.abr} File Type: {stream.mime_type.split("/")[1]}\n'
+            output.insert(END, StreamList)
+        else:
+                if stream.mime_type.split("/")[1] == "mp4":# and (stream.resolution == "720p" or stream.resolution == "1080p"):
+                    StreamList = f'itag: {stream.itag} Resolution: {stream.resolution} FPS: {stream.fps} File Type: {stream.mime_type.split("/")[1]}\n'
+                    output.insert(END, StreamList)
+    print(audio_flag) #for debugging purposes
 
 def close():
     window.destroy()
@@ -16,27 +26,28 @@ def close():
 
 
 window = Tk()
-window.title("Youtube Downloader")
-window.configure(background = "black")
+window.title("Youtube Downloader by Alykes")
+window.configure(background = "grey")
 
 check = IntVar()
+#check.set(True)
 
 logo = PhotoImage(file = "../assets/hacker.gif")
-Label (window, image = logo, bg = "black") .grid(row = 0, column = 0, sticky = W)
+Label (window, image = logo, bg = "grey") .grid(row = 0, column = 0, sticky = W)
 
-Label (window, text="Please enter the video URL:", bg = "black", fg = "white", font = "non 12 bold") .grid(row = 1, column = 0, sticky = W)
+Label (window, text="Please enter the video URL:", bg = "grey", fg = "black", font = "none 12") .grid(row = 1, column = 0, sticky = W)
 
-textbox = Entry(window, width = 50, bg = "white")
-textbox.grid(row = 2, column = 0, sticky = W)
+TextBox = Entry(window, width = 70, bg = "white")
+TextBox.grid(row = 2, column = 0, sticky = W)
 
-audiocheckbox = Checkbutton(window, text = "Filter - Audio Only", bg = "black", fg = "white", variable = check)
-audiocheckbox.grid(row = 3, column = 1, sticky = W)
-#audiocheckbox.pack()
+AudioCheckbox = Checkbutton(window, text = "Audio Only", bg = "grey", fg = "black", variable = check, onvalue = 1, offvalue = 0)
+AudioCheckbox.grid(row = 3, column = 1, sticky = W)
+#AudioCheckbox.pack()
 
-Button(window, text = "List Streams ", width = 12, command = dlit) .grid(row = 3, column = 0, sticky = W)
-Button(window, text = "Close", width = 10, command = close) .grid(row = 6, column = 0, sticky = E)
+Button(window, text = "List Streams ", width = 12, command = ListStreams) .grid(row = 3, column = 0, sticky = W)
+Button(window, text = "Close", width = 10, command = close) .grid(row = 7, column = 1, sticky = E)
 
-output = Text(window, width = 100, height = 20, wrap = WORD, background = "white")
+output = Text(window, width = 70, height = 20, wrap = WORD, background = "white")
 output.grid(row = 5, column = 0, columnspan = 2, sticky = W)
 
 window.mainloop()
